15655ad88204a3c023d2c3061fd8c28891208c76
Princess Ibeawuchi <priliz2454@gmail.com>
2023-02-21 17:21:04 -0800

    ./9-times_table | cat -e

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,39 +1,40 @@
 #include "main.h"
-/**
- * time_table - Prints the 9 time table, starting with 0 to 9
- * Return: empty output
- */
 
-void times_table(void)
-{
-	int x, y, u, z, d;
 
-	for (x = 0; x <= 9; x++)
+	/**
+	 * times_table - prints the 9 times table, starting with 0
+	 */
+	void times_table(void)
 	{
-		for (y = 0; y <= 9; y++)
+		int i, j, k;
+
+
+		for (i = 0; i < 10; i++)
 		{
-			z = x * y;
-			if (z > 9)
+			for (j = 0; j < 10; j++)
 			{
-				u = z % 10;
-				d = (z - u) / 10;
-				_putchar(44);
-				_putchar(32);
-				_putchar(d + '0');
-				_putchar(u + '0');
-			}
-			else
-			{
-				if (y != 0)
+				k = j * i;
+				if (j == 0)
 				{
-					_putchar(44);
-					_putchar(32);
-					_putchar(32);
+					_putchar(k + '0');
 				}
-				_putchar(z + '0');
+
+
+				if (k < 10 && j != 0)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(k + '0');
+				} else if (k >= 10)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((k / 10) + '0');
+					_putchar((k % 10) + '0');
+				}
 			}
+			_putchar('\n');
 		}
-		_putchar('\n');
 	}
-}
 
