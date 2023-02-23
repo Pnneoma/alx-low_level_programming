0e91c8cb95824829d3d3a4cf68e10c7ae9bdac5f
Princess Ibeawuchi <priliz2454@gmail.com>
2023-02-23 10:33:16 -0800

    100 prime numbers

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,25 @@
+#include <math.h>
+#include <stdio.h>
+
+/**
+ * main - Prints the Largest prime number of the number 612852475143
+ * return: Always 0 (success)
+ */
+
+int main(void)
+
+{
+	long l, maxnum;
+	long num = 612852475143;
+	double square = sqrt(num);
+
+	for (l = 1; l <= square; l++)
+	{
+		if (num % l == 0)
+		{
+			maxnum = num / l;
+		}
+	}
+	printf("%ld\n", maxnum);
+	return (0);
+}
