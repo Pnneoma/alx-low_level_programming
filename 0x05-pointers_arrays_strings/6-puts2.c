db71c49c97017df87b914655473dc6b0cc04be06
Princess Ibeawuchi <priliz2454@gmail.com>
2023-02-27 14:37:45 -0800

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * put2 - a function that prints every other character of a string,
+ * puts2 - a function that prints every other character of a string,
  * starting with the first character, followed by a new line.
  * @str: input
  * Return: print
