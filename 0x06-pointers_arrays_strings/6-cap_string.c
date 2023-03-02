8bc16b5526031eccdf17e0fd384bcbb4ed4714a2
Princess Ibeawuchi <priliz2454@gmail.com>
2023-03-02 15:23:47 -0800

    6-cap_string.c

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,7 +12,7 @@ char *cap_string(char *str)
 
 	while (str[index])
 	{
-		while (!(str[index] >= 'm' && str[index] <= 'n'))
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
 			index++;
 
 		if (str[index - 1] == ' ' ||
@@ -36,4 +36,3 @@ char *cap_string(char *str)
 
 	return (str);
 }
-
