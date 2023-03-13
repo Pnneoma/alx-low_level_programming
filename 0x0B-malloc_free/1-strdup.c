abadc82d27de33097ff744d7eb712fe54054e30c
Princess Ibeawuchi <priliz2454@gmail.com>
2023-03-13 06:48:06 -0700

    0x0B. C - malloc, free Mandatory

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * _strdup - duplicate to new memory space location
+ * @str: char
+ * Return: 0
+ */
+char *_strdup(char *str)
+{
+	char *aaa;
+	int a, b = 0;
+
+	if (str == NULL)
+		return (NULL);
+	a = 0;
+	while (str[a] != '\0')
+		a++;
+	aaa = malloc(sizeof(char) * (a + 1));
+	if (aaa == NULL)
+		return (NULL);
+	for (b = 0; str[b]; b++)
+		aaa[b] = str[b];
+	return (aaa);
+}
