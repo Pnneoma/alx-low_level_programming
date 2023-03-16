d27310ef06347247145e03e19a7f034441ebccda
Princess Ibeawuchi <priliz2454@gmail.com>
2023-03-16 01:05:53 -0700

    Task0 - Trust no one

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *malloc_checked - a function that allocates memory
+ * @b: number of bytes allocated
+ * Return: a pointer to the allocated memory
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+
+	if (p == NULL)
+		exit(98);
+	return (p);
+}
