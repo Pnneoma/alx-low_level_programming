c2ea3d05cb7fe46981e8513edf23ab89608cbee3
Princess Ibeawuchi <priliz2454@gmail.com>
2023-04-04 01:43:44 -0700

    0-print_listint0

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,13 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t nodes = 0;
+	size_t nodes_counts = 0;
 
-	while (h)
+	while (h != NULL)
 	{
-		nodes++;
+		nodes_counts++;
 		printf("%d\n", h->n);
 		h = h->next;
 	}
-	return (nodes);
+	return (nodes_counts);
 }
